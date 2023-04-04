8a754cdf755a04d806da000c796d9e49ab7740b5
Guilherme Felipe <guimota_silver@hotmail.com>
2023-04-04 10:59:23 -0300

    Atualizar as páginas

--- Program.cs
+++ Program.cs
@@ -2,6 +2,7 @@ using Microsoft.EntityFrameworkCore;
 using RazorPagesMovie.Model;
 using Microsoft.Extensions.DependencyInjection;
 using RazorPagesMovie.Data;
+using RazorPagesMovie.Models;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -22,6 +23,13 @@ else
 }
 
 var app = builder.Build();
+
+using (var scope = app.Services.CreateScope())
+{
+    var services = scope.ServiceProvider;
+
+    SeedData.Initialize(services);
+}
 
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
